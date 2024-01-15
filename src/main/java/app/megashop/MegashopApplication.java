77858ecf1c8246b42e397f7b3c0a2b57b0a3189c
Hassan Outhami <houthami@orsys.co.ma>
2024-01-15 15:26:27 +0100

    initial serveur

---
+++ src/main/java/app/megashop/MegashopApplication.java
@@ -0,0 +1,13 @@
+package app.megashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegashopApplication.class, args);
+	}
+
+}
